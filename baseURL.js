4981458d44659ff09c7d030fc00581e3b629b53f
rafihilmif <rafihilmifirdaus@gmail.com>
2024-10-27 15:51:43 +0700

    update

--- baseURL.js
+++ baseURL.js
@@ -1,3 +1,3 @@
 const baseURL = 'http://localhost:3030/api';
-
+// const baseURL = 'http://152.42.223.234:3030/api'
 export { baseURL };
--- baseURLFile.js
+++ baseURLFile.js
@@ -1,3 +1,3 @@
 const baseURLFile = 'http://localhost:3030/';
-
+// const baseURLFile = 'http://152.42.223.234:3030/'
 export { baseURLFile };
